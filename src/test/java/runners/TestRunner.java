c9d38fbf76715436c2b39f6f60b36abafe303246
Pankaj Sharma <pankaj33464@gmail.com>
2023-10-02 04:36:23 +1000

    Minor updates

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,18 +1,17 @@
 package runners;
 
 import io.cucumber.junit.CucumberOptions;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.Test;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import utils.DriverManager;
+import org.testng.annotations.Test;
 
-import static utils.DriverManager.driver;
-
-@CucumberOptions(tags = "@Sanity",
-        features = {"src/test/java/features"},
-        glue = {"stepDefinitions"},
-        plugin = {"html:target/results.html", "message:target/results.ndjson"}
-)
-public class TestRunner extends AbstractTestNGCucumberTests{
-
+public class TestRunner{
+    @CucumberOptions(tags = "@Sanity",
+            features = {"features"},
+            glue = {"stepDefinitions"},
+            plugin = {"html:target/results.html", "message:target/results.ndjson"},
+            monochrome = true
+    )
+    @Test
+    public class Run extends AbstractTestNGCucumberTests {
+    }
 }
