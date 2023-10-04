ad144b14d718506c80996ea1042fcbb76228ce4e
Pankaj Sharma <pankaj33464@gmail.com>
2023-10-05 02:18:23 +1000

    Feature file is moved to resources folder as it's a best practice to separate it out

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -1,15 +1,14 @@
 package runners;
 
-
 import io.cucumber.junit.Cucumber;
-import org.junit.runner.RunWith;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(tags = "@PositiveTest",
-        features = {"src/test/java/features"},
+        features = {"src/test/resources/features"},
         glue = {"stepDefinitions"},
-        plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+        plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
         monochrome = true)
 public class TestRunner {
 
